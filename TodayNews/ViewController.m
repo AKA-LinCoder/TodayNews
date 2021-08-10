0ac9db428430d1581b6839bc5fd14a961be6a6de
lsaac <2423860442@qq.com>
2021-08-10 20:56:15 +0800

    init

--- TodayNews/ViewController.m
+++ TodayNews/ViewController.m
@@ -15,6 +15,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UIView *view = [[UIView alloc] init]; //在内存空间申请初始化
+    
+    view.backgroundColor = [UIColor redColor];
+    view.frame  = CGRectMake(100, 100, 100,100);
+    [self.view addSubview: view];
+    
+    
+    UIView *view2 = [[UIView alloc] init];
+    view2.backgroundColor = [UIColor greenColor];
+    view2.frame = CGRectMake(100, 50, 100, 100);
+    [self.view addSubview: view2];
+    
+    
+//    [self.view addSubview:({
+//        UILabel *label = [[UILabel alloc] init];
+//        label.text = @"hello world";
+//        [label sizeToFit];
+//        label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+//        label;
+//    })];
     // Do any additional setup after loading the view.
 }
 
